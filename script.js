45c003ae907d0341d1bf72b4c15131fdc831755e
DanielAlinIosif <aliniosifdaniel@gmail.com>
2024-01-27 21:52:45 +0200

    git commit -m "

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const start = document.querySelector('.start');
-const screens = document.querySelectorAll('.screen');
+const screen = document.querySelectorAll('.screens');
 const timeList = document.querySelectorAll('.time-list');
 
 let time = 0;
 
 start.addEventListener('click', () => {
-    screens[0].classList.add('up')
+    screen[0].classList.add('up')
 });
 
 timeList.addEventListener('click', (event) => {
     if(event.target.classList.contains('time-btn')){
         time = parseInt(event.target.getAttribute('data-time'));
-        screens[1].classList.add('up')
+        screen[1].classList.add('up')
     }  
 });
