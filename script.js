69842f2966558cbec379a1e6cc3bba64dd56ea0b
DanielAlinIosif <aliniosifdaniel@gmail.com>
2024-01-28 05:40:58 +0200

    git commit -m "

--- script.js
+++ script.js
@@ -1,16 +1,60 @@
-const start = document.querySelector('.start');
-const screen = document.querySelectorAll('.screens');
+const start = document.querySelector('#start');
+const screens = document.querySelectorAll('.screen');
 const timeList = document.querySelectorAll('.time-list');
+const board = document.querySelector('#board');
+const timer = document.querySelector('#time');
+let interval = null;
 
 let time = 0;
 
 start.addEventListener('click', () => {
-    screen[0].classList.add('up')
+    screens[0].classList.add('up')
 });
 
 timeList.addEventListener('click', (event) => {
     if(event.target.classList.contains('time-btn')){
         time = parseInt(event.target.getAttribute('data-time'));
-        screen[1].classList.add('up')
+        screens[1].classList.add('up');
+        startGame();
     }  
 });
+
+function startGame() {
+    interval = setInterval(decreaseTime, 1000);
+    createRandomCircle();
+    setTimer(time)
+}
+
+function finishGame() {
+
+}
+
+function decreaseTime() {
+    if(time === 0) {
+        finishGame();
+    } else {
+        --time;
+        setTimer(time)
+    }
+}
+
+function setTimer(value) {
+    if(value < 10) {
+        timer.innerHTML = '00:0${value}';
+    } else {
+        timer.innerHTML = '00:${time}';
+    }
+}
+
+
+function createRandomCircle() {
+    const circle = document.createElement('div');
+    
+    circle.style.width = 16px;
+    circle.style.height = 16px;
+
+}
+
+// temporary
+time = 10;
+startGame();
